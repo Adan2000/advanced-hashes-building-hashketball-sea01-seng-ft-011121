26d0e86cce111a9cbe0bc4b04a04d5acfaeedc01
Adan Escamilla <adanescamilla580@gmail.com>
2021-01-02 19:42:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,34 @@
-# Write your code here!
+
+require 'pry'
+
+def game_hash 
+
+ 
+ new_hash = {
+   :home => {
+     :team_name => "Brooklyn Nets",
+     :colors => "Black, White",
+     :players => [
+       :player_name => "Alan Anderson", "Reggie Evans"
+       :number => 0, 30,
+       :shoe => 16, 14,
+       :points => 22, 12,
+       :rebounds => 12, 12, 
+       :assists => 12, 12, 
+       :steals => 3, 12, 
+       :blocks => 1, 12, 
+       :slam_dunks => 1, 7, 
+   ]
+     
+   },
+   :away => {
+     :team_name => {},
+     :colors => {},
+     :players => []
+   }
+ }
+
+
+
+
+end
