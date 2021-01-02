c87aa3789846d1daf1a925da4afda806e0ec9698
Adan Escamilla <adanescamilla580@gmail.com>
2021-01-02 19:55:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,9 +8,9 @@ def game_hash
    :home => {
      :team_name => "Brooklyn Nets",
      :colors => ["Black, White"],
-     :players =>  [
+     :players =>  [{
        
-       ] 
+       }] 
       },
    :away => {
      :team_name => {},
