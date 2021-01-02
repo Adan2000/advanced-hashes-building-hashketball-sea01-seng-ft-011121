cc17634d60d63116bab36eea2615cb60a3702fc0
Adan Escamilla <adanescamilla580@gmail.com>
2021-01-02 19:47:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,14 +10,14 @@ def game_hash
      :colors => "Black, White",
      :players => [
        :player_name => {},
-       :number => {},
-       :shoe => {},
-       :points => {}
-       :rebounds => {},
-       :assists => {},
-       :steals => {},
-       :blocks => {},
-       :slam_dunks =>{},
+       :number 
+       :shoe 
+       :points 
+       :rebounds 
+       :assists 
+       :steals 
+       :blocks 
+       :slam_dunks 
    ]
      
    },
